cb62d66ab87f4b4c1232f2b6ece307d48dfb6a2a
[Github <[Github>
2021-09-02 15:18:09 +0900

    5장(함수와 이벤트)

---
+++ 5장(함수와 이벤트)/js/event.js
@@ -0,0 +1,8 @@
+function showDetail(){
+    document.querySelector("#desc").style.display= "block";
+    document.querySelector("#open").style.display= "none";
+}
+function hideDetail(){
+    document.querySelector("#desc").style.display= "none";
+    document.querySelector("#open").style.display= "block";
+}
